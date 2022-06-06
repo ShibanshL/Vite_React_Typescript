054748e7ee7c7f2336b55876245dbc98bc351920
shibanshL <shibansh.w@gmail.com>
2022-06-06 20:01:11 +0530

    Started working with Mantine

--- src/Test/Test.tsx
+++ src/Test/Test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Test.css'
-import { Grid } from '@mantine/core';
+import { Center } from '@mantine/core';
 import { Input } from '@mantine/core';
 import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
 // import { At } from 'tabler-icons-react';
@@ -12,63 +12,10 @@ function Test() {
       : theme.colors.gray[7];
   
     return (
-        <Grid>
-            <Grid.Col span={6}>
-                <div className='Test' style={{ width: 340, margin: 'auto', display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column', background:'lightblue' }}>
-                    <Card shadow="sm" p="lg">
-                    <Card.Section>
-                        <Image src="https://ipt.imgix.net/202004/x/0/lofoten-216.jpg" style={{marginLeft:'3vw'}} height={180} alt="Norway" />
-                    </Card.Section>
-            
-                    <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm, padding:'0px 25px' }}>
-                        <Text weight={500}>Norway Fjord Adventures</Text>
-                        <Badge color="pink" variant="light">
-                        On Sale
-                        </Badge>
-                    </Group>
-            
-                    <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5, padding:'0px 25px', marginTop:'2vh' }}>
-                        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                        activities on and around the fjords of Norway
-                    </Text>
-            
-                    <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
-                        Book classic tour now
-                    </Button>
-                    </Card>
-                </div>
-            </Grid.Col>
-            <Grid.Col span={6}>
-                    <div className='Test' style={{ width: 340, margin: 'auto', display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column', background:'lightblue' }}>
-                        <Card shadow="sm" p="lg">
-                        <Card.Section>
-                            <Image src="https://ipt.imgix.net/202004/x/0/lofoten-216.jpg" style={{marginLeft:'3vw'}} height={180} alt="Norway" />
-                        </Card.Section>
-                
-                        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm, padding:'0px 25px' }}>
-                            <Text weight={500}>Norway Fjord Adventures</Text>
-                            <Badge color="pink" variant="light">
-                            On Sale
-                            </Badge>
-                        </Group>
-                
-                        <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5, padding:'0px 25px', marginTop:'2vh' }}>
-                            With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                            activities on and around the fjords of Norway
-                        </Text>
-                
-                        <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
-                            Book classic tour now
-                        </Button>
-                        </Card>
-                    </div>
-            </Grid.Col>
-      
-      
-
-      {/* <Grid.Col span={4}>3</Grid.Col> */}
-    </Grid>
-    );
+            <Center style={{ width:'90vw', height:'90vh', borderRadius:'25px', backdropFilter:'blur(10px)' }}>
+              <div>All elements inside Center are centered</div>
+            </Center>
+    );       
 }
 
 export default Test

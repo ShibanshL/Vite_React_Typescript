92e9c722a729251fd1fbdce82e243765daf65d5b
shibanshL <shibansh.w@gmail.com>
2022-06-07 12:15:08 +0530

    Working on the UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Display from './Component/Display';
 import Sub from './Component/Sub';
 import Sub_1 from './Component_2/Sub_1';
 import Test from './Test/Test';
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme, Transition } from '@mantine/core';
 
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
   return (
     <div className="App">
       {/* <div className="Search"><Sub /></div> */}
-      <div className="Search"><Test /></div>
-      {/* <div className="Search"><Sub_1 /></div> */}
+      {/* <div className="Search"><Test /></div> */}
+      <div className="Search"><Sub_1 /></div>
 
 
      

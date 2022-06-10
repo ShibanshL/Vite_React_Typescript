9ec18d7fbd317581b39e6c2cffffc2cc77e1b0a3
shibanshL <shibansh.w@gmail.com>
2022-06-10 12:43:53 +0530

    Removed all the code, gonna start from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,13 @@ function App() {
 
   return (
     <AppShell
-      // padding="md"
-      // navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
-      // header={<Header height={60} p="xs">{/* Header content */}</Header>}
+
       styles={(theme) => ({
-        main: { /*backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],*/
-                height:'100vh', width:'100vw', display:'flex', alignItems:'center', justifyContent:'center', padding:0, margin:0, boxSizing:'border-box',
+        main: { height:'100vh', width:'100vw', display:'flex', alignItems:'center', justifyContent:'center', padding:0, margin:0, boxSizing:'border-box',
               background:'url(https://ipt.imgix.net/202004/x/0/lofoten-216.jpg)', overflow:'hidden' },
       })}
     >
       <Container size='xl' style={{ display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column'}}><Sub_1 /></Container>
-      {/* <Container size='xl'><Test /></Container> */}
-
-      
-      {/* Your application here */}
     </AppShell>
   );
 }
